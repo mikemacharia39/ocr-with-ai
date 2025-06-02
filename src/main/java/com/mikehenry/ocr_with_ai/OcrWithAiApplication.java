3ad9f5d822a7480938decb0b96e0f31c270ae479
mikemacharia39 <mikemacharia39@gmail.com>
2025-06-02 20:16:57 +0300

    spring boot project to read documents via ai

---
+++ src/main/java/com/mikehenry/ocr_with_ai/OcrWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.mikehenry.ocr_with_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikehenry/ocr_with_ai/OcrWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikehenry.ocr_with_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
